a09254f4cf33af3842756c2933593cfde6b427e2
aalonso <alvar.alonso@st.ovgu.de>
2020-01-16 19:37:01 +0100

    add message

--- code.js
+++ code.js
@@ -1,7 +1,10 @@
 
 var server = new SillyClient();
-server.connect("wss://tamats.com:55000", "TestChat");
 
+var inputMessage = document.getElementById("message-input");
+var addButton = document.getElementById("add");
+
+server.connect("wss://tamats.com:55000", "TestChat");
 server.on_ready = isConnected;
 
 function isConnected ()
@@ -9,6 +12,18 @@ function isConnected ()
     console.log("You have connected to the server!");
 }
 
+addButton.addEventListener('click', addMessage);
+
+function addMessage(){
+	if(inputMessage.value != " "){
+		var newElement = document.createElement("LI");
+		var text = document.createTextNode(inputMessage.value);
+		newElement.appendChild(text);
+
+		document.getElementById("messageList").appendChild(newElement);
+	}
+}
+/*
 document.getElementById("add").onclick = function()
 {
     var text = document.getElementsByName("msg").value;
@@ -17,3 +32,4 @@ document.getElementById("add").onclick = function()
     console.log(list);
     document.getElementById("message-container").appendChild(list);
 }
+*/
