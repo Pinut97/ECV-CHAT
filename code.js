856b80d5059367554afac7d10286c52cac87501b
Pinut97 <pau.florenti01@estudiant.upf.edu>
2020-01-17 20:13:36 +0100

    Messages

--- code.js
+++ code.js
@@ -3,14 +3,35 @@ var server = new SillyClient();
 
 var inputMessage = document.getElementById("message-input");
 var addButton = document.getElementById("add");
+var userList = [];
 
-server.connect("wss://tamats.com:55000", "TestChat");
+server.connect("wss://ecv-etic.upf.edu/node/9000/ws", "TestRoom");
+
+/*
+class User {
+    user(name, id)
+    {
+        this.name = name;
+        this.id = id;
+    }
+};
+
+server.on_user_connected = function()
+{
+    var newUser = user(name, userList.length());
+    userList.push(newUser);
+    greetings(userList.indexOf(newUser).id)
+};
+*/
+
+//server.connect("wss://tamats.com:55000", "TestChat");
+
 server.on_connect = greetings;
 server.on_ready = isConnected;
 
-function greetings()
+function greetings(id)
 {
-    var greet = document.createTextNode("Welcome to the chat dear user!");
+    var greet = document.createTextNode("Welcome to the chat user " + id +"!");
     var newElement = document.createElement("LI");
     newElement.appendChild(greet);
     document.getElementById("messageList").appendChild(newElement);
@@ -20,17 +41,27 @@ function greetings()
 function isConnected ()
 {
     console.log("You have connected to the server!");
+    console.log(userList);
 }
 
-addButton.addEventListener('click', addMessage);
+server.on_message = function(id, msg)
+{
+    var message = document.createTextNode( msg );
+    var li = document.createElement("LI")
+    li.appendChild(message);
+    document.getElementById("messageList").appendChild(li);
+}
 
-function addMessage(){
-	if(inputMessage.value != " "){
-		var newElement = document.createElement("LI");
-        var text = document.createTextNode(inputMessage.value);
-		newElement.appendChild(text);
-        server.sendMessage(inputMessage.value);
-        inputMessage.value = " ";
-		document.getElementById("messageList").appendChild(newElement);
-	}
-};
+document.getElementById("add").onclick = function()
+{
+    if(!server || !server.is_connected)
+    {
+        return;
+    }
+    var text = document.getElementById("message-input").value;
+    var li = document.createElement("li");
+    li.textContent = text;
+    server.sendMessage(text);
+    document.getElementById("messageList").appendChild(li);
+    document.getElementById("message-input").value = "";
+}
