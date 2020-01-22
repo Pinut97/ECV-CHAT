23f37b42229d47a0cd8332904031bc1a2f4a375d
Pinut97 <pau.florenti01@estudiant.upf.edu>
2020-01-22 22:15:33 +0100

    Users appear when messaging

--- code.js
+++ code.js
@@ -1,30 +1,36 @@
 
 var server = new SillyClient();
 
-server.close();
-
-function login ()
+function login()
 {   
     var room = document.getElementById("Room").value;
     var name = document.getElementById("UserName").value;
+    server.close();
     server.connect("wss://ecv-etic.upf.edu/node/9000/ws", room);
-    server.on_ready = greetings(name);
-}
+    server.on_connect = greetings(name);
+    server.on_ready = userReady;
+};
+
+function userReady()
+{
+    name = document.getElementById("UserName").value;
+    server.user_name = name;
+    server.sendMessage("User " + server.user_name + " has connected!");
+};
 
 function greetings(name)
 {
     server.user_name = name;
-    console.log(server.user_name);
-    var greet = document.createTextNode("Welcome to the chat user " + name +"!");
+    var greet = document.createTextNode("Welcome to the chat user " + server.user_name +"!");
     var newElement = document.createElement("LI");
     newElement.appendChild(greet);
     document.getElementById("messageList").appendChild(newElement);
-    server.on_ready = server.sendMessage("User " + server.user_name + " has connected to the room.");
-}
+};
 
-server.on_user_connected = function( user_id )
+server.on_user_connected = function( user_id, data)
 {
-    console.log("User with id: " + user_id + " has connected.");
+    console.log("user connected");
+    console.log("User with id: " + server.user_name + " has connected.");
 }
 
 server.on_message = function(id, msg)
