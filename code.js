72c31d82bff4bfdcc66ee13f2c080cd1587dffff
Pinut97 <pau.florenti01@estudiant.upf.edu>
2020-01-23 21:34:38 +0100

    simplified and solved previous unordered msg

--- code.js
+++ code.js
@@ -20,9 +20,10 @@ function userReady()
     var text = "User " + server.user_name + " has connected!";
     var message = {
         username: server.user_name,
-        type: "msg-login",
+        type: "msg",
         data: text,
-        login: true
+        login: true,
+        id: server.user_id
     };
     server.sendMessage(JSON.stringify(message));
 };
@@ -39,48 +40,37 @@ function greetings()
 
 server.on_user_connected = function( user_id, data )
 {
-    var message = {
-        username: server.user_name,
-        type: "id",
-        data: server.user_id
-    };
 
-    server.sendMessage(JSON.stringify(message), user_id);
 };
 
 server.on_message = function(id, msg)
 {  
     var parsed = JSON.parse(msg);
-    if (parsed.type === "id")
-    {
-        server.room["clients"].push(parsed.data); //afegeix el nou id a la llista de clients.
-        server.room["updated"] = true; //L'usuari està updated ja que si rep aquest missatge significa que ell té tots els anteriors missatges.
-        server.room["clients"].sort(function(a, b){return a-b}); //Ordena la lista de forma ascendent.
-        getPreviousMessages(server.room["clients"][0]);
-    } 
-    else if (parsed.type === "msg") 
-    {
-        if(parsed.login === true){
-            return;
-        }
-        messageStore.push(parsed);
-        var message = document.createTextNode( "[" + parsed.username + "]: " + parsed.data );
-        var li = document.createElement("LI")
-        li.appendChild(message);
-        document.getElementById("messageList").appendChild(li);
-    }
-    else if (parsed.type === "update_petition") 
-    {
-        sendPreviousMessages(id);
-    }
-    else if (parsed.type === "update") 
-    {
-        var message = document.createTextNode( "[" + parsed.username + "]: " + parsed.data );
-        var li = document.createElement("LI")
-        li.appendChild(message);
-        document.getElementById("messageList").appendChild(li);
-    }
 
+    if (parsed.type === "msg") 
+    {
+        if(parsed.login === true)
+        {
+            var message = document.createTextNode( "[" + parsed.username + "]: " + parsed.data );
+            var li = document.createElement("LI")
+            li.appendChild(message);
+            document.getElementById("messageList").appendChild(li);
+            server.room.updated = true;
+            server.room.clients.sort(function(a, b){return a-b});
+            if(server.user_id == server.room.clients[0])
+            {
+                sendPreviousMessages(parsed.id);
+            }
+        }
+        else
+        {
+            messageStore.push(parsed);
+            var message = document.createTextNode( "[" + parsed.username + "]: " + parsed.data );
+            var li = document.createElement("LI")
+            li.appendChild(message);
+            document.getElementById("messageList").appendChild(li); 
+        }
+    }
 };
 
 function sendMsg()
@@ -109,23 +99,8 @@ function sendMsg()
 
 function sendPreviousMessages(id) 
 {
-    var list = document.getElementById("messageList");
-    var length = document.getElementById("messageList").getElementsByTagName("li").length;
-    var output = [];
-
     for(var i = 0; i < messageStore.length; i++ )
     {
         server.sendMessage(JSON.stringify(messageStore[i]), id);
     }
-}
-
-function getPreviousMessages(id) {
-
-    var message = {
-        username: server.user_name,
-        type: "update_petition",
-        data: ""
-    };
-
-    server.sendMessage(JSON.stringify(message), id);
 }
