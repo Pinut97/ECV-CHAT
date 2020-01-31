d27eb402b708e7a00cfc805a97db54f1f900b051
Pinut97 <pau.florenti01@estudiant.upf.edu>
2020-01-31 11:02:27 +0100

    First try on websocket

--- assignment2/server.js
+++ assignment2/server.js
@@ -1,4 +1,40 @@
 
+var webSocketServer = require('websocket').server;
+var http = require('http');
+
+var ourPort = 9022;
+var clients = [];
+
+var server = http.createServer(function(request, response)
+{
+
+});
+
+server.listen(ourPort, function()
+{
+	console.log("Server listening to port: " + ourPort);
+});
+
+wsServer = new webSocketServer({
+	httpServer: server
+});
+
+wsServer.on('request', function(request)
+{
+	var connection = request.accept(null, request.origin);
+
+	connection.on('message', function(message)
+	{
+
+	});
+
+	connection.on('close', function(connection)
+	{
+
+	});
+});
+
+/*
 var http = require('http');
 var url = require('url');
 
@@ -7,8 +43,9 @@ var server = http.createServer( function(request, response) {
 	var url_info = url.parse( request.url, true ); //all the request info is here
 	var pathname = url_info.pathname; //the address
 	var params = url_info.query; //the parameters
-	response.end("OK!"); //send a response
+	response.end("OK! User connected"); //send a response
 });
 server.listen(9022, function() {
 	console.log("Server ready!" );
 });
+*/
